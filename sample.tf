2f0db4d09b15a8a4d34f0f8796224453db4955ae
sahil <sahilgawade612@gmail.com>
2024-02-13 18:49:58 +0530

    adeed

--- sample.tf
+++ sample.tf
@@ -2,8 +2,6 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-
-
 # Launch AWS Instance
 resource "aws_instance" "my_instance" {
     ami = var.image_id
@@ -12,18 +10,6 @@ resource "aws_instance" "my_instance" {
     vpc_security_group_ids = [ "sg-0ca2ae9bf1a0f4713"]
     tags = {
         Name = "my-instance"
-        env = "dev"
-    }
-}
-
-
-resource "aws_instance" "another_instance" {
-    ami = var.image_id
-    instance_type = var.machine_type
-    key_name = "new-universal-key"
-    vpc_security_group_ids = ["sg-0ca2ae9bf1a0f4713"]
-    tags = {
-        Name = "another-instance"
         env = "dev"
     }
 }
