854152c9849e39410d73c36c1946a0ddc4f6bf43
sahil <sahilgawade612@gmail.com>
2024-02-14 16:26:40 +0530

    adeed

---
+++ day2/homework/user.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_iam_user" "lb" {
+     name = "loadbalancer"
+     path = "/system/"
+
+  tags = {
+     tag-key = "tag-value"
+  }
+}
