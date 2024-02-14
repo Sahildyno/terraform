201e8701a5966b1f337a76faedfcbce67c083766
sahil <sahilgawade612@gmail.com>
2024-02-14 14:57:41 +0530

    adeed

---
+++ day2/output.tf
@@ -0,0 +1,15 @@
+output "hello_world" {
+    value = "hello_world. welcome to cloudblitz"
+}
+
+output "public-instance-public-ip" {
+    value = aws_instance.instance_2.public_ip
+}
+
+output "public-instance-state" {
+    value = aws_instance_2.instance_state
+}
+
+output "public-instance-state" {
+    value = aws_instance.instance_1.instance_state
+}
---
+++ day2/variable.tf
@@ -0,0 +1,31 @@
+variable "vpc_cidr" {
+    default = "192.168.0.0/16"
+}
+
+variable "private_subnet_cidr" {
+    default = "192.168.0.0/20"
+}
+
+variable "public_subnet_cidr" {
+    default = "192.168.0.0/20
+}
+
+variable "project" {
+    default = "cloudblitz"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "image_id" {
+    default = "ami-03f4878755434977f"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "key_pari" {
+    default = "new-universal-key"
+}
