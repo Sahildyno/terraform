283bcaf65f0c386bb2f7612148b2a9c9b3f78682
sahil <sahilgawade612@gmail.com>
2024-02-22 17:21:36 +0530

    nsjklh

--- day4/main.tf
+++ day4/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
     backend "s3" {
         region = "ap-south-1"
-        bucket = "test-1"
+        bucket = "mobile-1"
         key = "./terraform.tfstate"
     }
 }
